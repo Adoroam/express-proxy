c81a91887659d8ac7635b24bd3d8822a184402a2
Anthony Stabile <astabile.design@gmail.com>
2016-12-14 19:57:58 -0600

    reverted to old style using new proxySite object

--- start.js
+++ start.js
@@ -1,41 +1,38 @@
 'use strict';
-const express = require('express');
-const base = express();
-const bodyParser = require('body-parser');
+var http = require('http'); //get the http module
+var httpProxy = require('http-proxy'); //get the proxy module
 
-function proxySite(hn, pn, df){
+var proxy = httpProxy.createProxyServer();//create the server when proxy is called
+
+function proxySite(hn, pn){
 	this.hostname = hn;
 	this.port = pn;
-	if(df) {
-		this.dist = df;
-	}	else {
-		this.dist = 'dist';
-	};
-	this.app = express();
 };
 var sites = [];
 //add sites here
 sites[0] = new proxySite('asepdesign.com', 8001);
 sites[1] = new proxySite('hotswapheroes.com', 8002);
 
-sites.forEach(function(ele){
-	ele.app.use(express.static(ele.dist));
-	ele.app.use(bodyParser.urlencoded({ extended:false }));
-	ele.app.use(bodyParser.json());
-	ele.app.get('/', function(req, res){
-		res.redirect('/');
-	});
-	ele.app.get('/*', function(req, res){
-		var pr = req.params["0"];
-		res.redirect('/?in='+pr);
-	});
-});
+//this creates a regular server and listens on port 80 (default web traffic port)
+http.createServer(function (req, res) {
+    //this is the virtual host function that creates a new child process using the given port
+    function vhost(port) {
+        proxy.web(req, res,{
+            target: 'http://localhost:'+port
+        });
+    };
+    //this statement checks the url header to find where the request is coming from
+    switch(req.headers.host) {
+        case sites[0].hostname:
+        vhost(sites[0].port);
+        break;
+        case sites[1].hostname:
+        vhost(sites[1].port);
+        break;
+        default:
+        vhost(sites[0].port);
+    };
 
-base.listen(80, function(){
-	console.log("base ready on port 80");
-	sites.forEach(function(ele){
-		ele.app.listen(ele.port, function(){
-			console.log(ele.hostname+" listening on port: "+ele.port);
-		});
-	});
+}).listen(80, function(){
+    console.log("proxy listening on port 80");
 });

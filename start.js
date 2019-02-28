f6743a65340689c0450c982e6db813dd6521061f
Anthony Stabile <astabile@texasfirelogix.com>
2019-02-28 14:29:07 -0600

    set up redirect branch for use with tfl

--- start.js
+++ start.js
@@ -1,18 +1,29 @@
 'use strict'
-const http = require('http') // get the http module
-const httpProxy = require('http-proxy') // get the proxy module
-const sites = require('./sites')
+const http = require('http')
+const https = require('https');
+const httpProxy = require('http-proxy')
+const fs = require('fs')
 
 var proxy = httpProxy.createProxyServer()// create the server when proxy is called
 
+const aPort = 80
+const bPort = 443
+
+const bOpts = {
+  key: fs.readFileSync('/etc/letsencrypt/live/app.texasfirelogix.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/app.texasfirelogix.com/fullchain.pem')
+}
+
+
 // this creates a regular server and listens on port 80 (default web traffic port)
-http.createServer(function (req, res) {
-// this is the virtual host function that creates a new child process using the given port
-  function vhost (port) {
-    proxy.web(req, res, { target: `http://localhost:${port}` })
-  }
-  let found = sites.find(site => site.hostname === req.headers.host)
-  found ? vhost(found.port) : vhost(sites[0].port)
-}).listen(80, function () {
-  console.log('proxy listening on port 80')
-})
+http.createServer((req, res) => {
+  const vhost = port => proxy.web(req, res, { target: `http://localhost:${port}` })
+  vhost(5151)
+  // req.headers.host
+}).listen(aPort,() => { console.log('http proxy listening on port ', aPort) })
+
+https.createServer(bOpts, (req, res) => {
+  const vhost = port => proxy.web(req, res, { target: `https://localhost:${port}` })
+  vhost(5151)
+  // req.headers.host
+}).listen(bPort, () => { console.log('https proxy listening on port ', bPort) })
